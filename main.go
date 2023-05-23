3808acf9aa8616e16d9c495451078f8732d11de7
emanzat <emanzat@users.noreply.github.com>
2023-05-23 11:29:54 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.RemoteAddr, r.Method, r.URL.String())
 
-	fmt.Fprintf(w, "eeeeeeeeeeeeeeeeeeeee \n")
+	fmt.Fprintf(w, "aaaaaaaaaaaaaaaaaaaaaaaaa \n")
 }
 
 func main() {
